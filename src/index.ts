94b77d4bb257d751c3a34051dbf38bf21366d712
Kaan Ersoy <hizir8899@gmail.com>
2022-03-30 19:48:48 +0300

    add func for decrease coverage score (#4)
    * add coverage tool
    
    * add secret for codecov
    
    * add test codecov step
    
    * add automated code coverage badge
    
    * add test function for decrease coverage score

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export const sum = (a: number, b: number) => a + b;
 
 export const split = (a: string) => a.split(",");
+
+export const letMeScrewUpYourCoverageScore = () => console.log("hell yeah");
